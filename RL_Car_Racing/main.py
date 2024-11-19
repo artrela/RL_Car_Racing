82c6e118177aeada683bdc10591bc94264d13e21
artrela <trelaalec@gmail.com>
2024-11-19 14:52:21 -0500

    Safety Push: stuck on dqn forward pass

--- RL_Car_Racing/main.py
+++ RL_Car_Racing/main.py
@@ -1,19 +1,31 @@
 import gymnasium as gym
 from models.dqn import DQNAgent
+from gymnasium.wrappers import GrayscaleObservation, FrameStackObservation, TransformObservation, numpy_to_torch
+import torch
     
 NUM_EPISODES = 100
-MEM_LEN = 1e5
+MEM_LEN = int(1e5)
 SEED = 1
+BATCH_SIZE = 32
 
 def train(config: str):
 
-    env = gym.make("CarRacing-v3", obs_type="")
+    env = gym.make("CarRacing-v3")
 
-    agent = DQNAgent(env, memory_length=MEM_LEN)
+
+    agent = DQNAgent(env, memory_size=MEM_LEN, batch_size=BATCH_SIZE)
+    env = numpy_to_torch.NumpyToTorch(env, device=agent.q_net.device)
+    env = TransformObservation(env, lambda x: torch.permute(x, (2, 0, 1)), env.observation_space)
+
+    print("Using device:", agent.q_net.device)
 
     terminated = truncated = False
 
     prev_observation, info = env.reset(seed=SEED)
+    prev_observation = prev_observation.float()
+
+    action = env.action_space.sample()
+    action = torch.tensor(action).float().to(agent.q_net.device)
 
     for e in range(NUM_EPISODES):
 
@@ -22,8 +34,10 @@ def train(config: str):
         while not terminated or truncated:
 
             observation, reward, terminated, truncated, info = env.step(action)
+            observation = observation.float()
 
-            action = agent(action, prev_observation, observation, reward, terminal=terminated or truncated)
+            action = agent(prev_observation, action, reward, observation, terminated or truncated)
+
             
             if terminated or truncated:
                 prev_observation, info = env.reset(seed=SEED)
@@ -33,4 +47,4 @@ def train(config: str):
     env.close()
 
 if __name__ == "__main__":
-    train()
+    train("")
--- RL_Car_Racing/models/dqn.py
+++ RL_Car_Racing/models/dqn.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class DQNAgent():
-    def __init__(self, env, memory_length, exp_size):
+    def __init__(self, env, memory_size, batch_size):
 
         self.total_steps = 0 
         self.current_episode = 0
@@ -13,12 +13,13 @@ class DQNAgent():
 
         self.epsilon, self.end_exploration = 0.1, 1e6
         
-        self.exp_replay = ExperienceReplay(memory_length, exp_size)
-        
+        self.exp_replay = ExperienceReplay(memory_size)
+        self._batch_size = batch_size
+
         self.env = env   # experience replay parameters
 
-        self.QNetwork = QNetwork(action_space=env.action_space.shape)
-        self.optim = torch.optim.AdamW(params=self.QNetwork.parameters(), lr=0.001)
+        self.q_net = QNetwork(action_space=env.action_space.shape[0])
+        self.optim = torch.optim.AdamW(params=self.q_net.parameters(), lr=0.001)
         self.loss_fn = torch.nn.MSELoss()
 
     def __call__(self, s0, a0, r0, s1, t): 
@@ -26,16 +27,18 @@ class DQNAgent():
         # store environment interactions into the experience buffer
         self.exp_replay.storeExperience(s0, a0, r0, s1, t)
 
+        self.epsilon = self._epsilonDecay()
+
         # select a next action with an epsilon greedy strategy
-        next_action = self._selectAction(self.epsilon)
+        next_action = self._selectAction()
         
         if self.current_episode % self.network_update == 0:
 
-            experiences = self.exp_replay.getRandomExperiences()
+            experiences = self.exp_replay.getRandomExperiences(self._batch_size)
             
             targets, states = self._prepareMinibatch(experiences)
             
-            q_pred = self.QNetwork(states)
+            q_pred = self.q_net(states)
             losses = self.loss_fn(targets, q_pred)
 
             self.optim.zero_grad()
@@ -47,25 +50,25 @@ class DQNAgent():
         return next_action
     
     def _prepareMinibatch(self, experiences):
-        pass
+        
+        breakpoint()
+        targets = torch.concat(experiences.actions, dim=0)
     
 
     def _epsilonDecay(self):
-        
-        new_eps = (1 - self.eps) / self.end_exploration * self.current_episode
-        return new_eps
+        return (1 - self.epsilon) / self.end_exploration * self.current_episode
 
 
-    def _selectAction(self, eps):
+    def _selectAction(self):
         
         P = random.random()
-        if P < 1 - eps:
+        if P < 1 - self.epsilon:
             with torch.no_grad(): 
-                state = self.exp_replay.getCurrentExperience()
-                actions = self.QNetwork(state.to(self.QNetwork.device)) 
+                state = self.exp_replay.getCurrentExperience().state
+                actions = self.q_net(state.to(self.q_net.device)) 
                 action = torch.argmax(actions)
         else:
-            action = self.env.action_space.sample()
+            action = torch.tensor(self.env.action_space.sample()).float()
 
         return action
     
@@ -82,61 +85,41 @@ class DQNAgent():
 
 class QNetwork(torch.nn.Module):
     def __init__(self, action_space):
+        super().__init__()
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model =  torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=8, stride=4),
+        self.model =  torch.nn.Sequential(*[
+            torch.nn.Conv2d(in_channels=3, out_channels=16, kernel_size=8, stride=4),
             torch.nn.ReLU(),
             torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2),
             torch.nn.ReLU(),
             torch.nn.Linear(in_features=13824, out_features=256),
             torch.nn.ReLU(),
             torch.nn.Linear(in_features=256, out_features=action_space)
-        ).to(self.device)
+        ]).to(self.device)
 
     def forward(self, x):
         return self.model(x)
 
 
 class ExperienceReplay():
-    def __init__(self, batch_size, memory_length, state_length):
+    def __init__(self, memory_length):
 
         self.Experience = namedtuple("Experience", ["state", "reward", "action", "next_state", "terminal"])
         self._replay_memory = deque(maxlen=memory_length)
 
-        self._current_state = deque(maxlen=state_length)
-        self._next_state = deque(maxlen=state_length)
-        self.state_length = state_length
-
-        self._batch_size = batch_size
-
-    def _createStates(self, ss0, ss1):
-
-        self._current_state.append(ss0)
-        self._next_state.appendleft(ss1)
-
-        while self._current_state < self.state_length:
-            self._current_state.appendleft(torch.zeros_like(ss0))
-        
-        while self._next_state < self.state_length:
-            self._current_state.append(torch.zeros_like(ss1))
-
-        return torch.stack(self._current_state, dim=-1), torch.stack(self._next_state, dim=-1)
-
-    def storeExperience(self, ss0, a0, r0, ss1, t):
-
-        s_in, s_out = self._createStates(ss0, ss1)
+    def storeExperience(self, s0, a0, r0, s1, t):
 
         if len(self._replay_memory) == self._replay_memory.maxlen:
             self._replay_memory.popleft()
 
-        new_experience = self.Experience(s_in, a0, r0, s_out, t)
+        new_experience = self.Experience(s0, a0, r0, s1, t)
         self._replay_memory.append(new_experience)
 
         return
 
-    def getRandomExperiences(self):
-        return random.sample(self._replay_memory, k=self._batch_size)
+    def getRandomExperiences(self, batch_size):
+        return random.sample(self._replay_memory, k=batch_size)
     
     def getCurrentExperience(self):
         return self._replay_memory[-1]
